5c48b3fac0076a82f47f0b5eb298eede7ce32a09
Berto Moore <30520062+bertomoore@users.noreply.github.com>
2021-01-29 07:13:36 -0700

    Update Demos.cs

--- Lesson-10-Final-Project/Demos.cs
+++ Lesson-10-Final-Project/Demos.cs
@@ -14,7 +14,7 @@ namespace Demos
 
             List<Employee> employees = new List<Employee>();
 
-            for (int i = 0; i < 2; i++)
+            for (int i = 0; i < 10; i++)
             {
                 string idNumber, state;
                 decimal hoursWorked, hourlyRate;
